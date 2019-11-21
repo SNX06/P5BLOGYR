ebf7113ed7f92f1daca58865cf6b2b344aebd122
Yannis Rekkas <yannis.rekkas@gmail.com>
2019-11-21 13:25:07 +0100

    Initial commit

---
+++ model/DbManager.php
@@ -0,0 +1,47 @@
+<?php 
+class DbConnect
+{
+    private $connec_host = 'localhost';
+    private $connec_dbname = 'p5blog';
+    private $connec_pseudo = 'root';
+    private $connec_mdp = '';
+
+    public function GetDb($Query)
+    {
+        $Db=$this->connexion();
+        $Req = $Db->prepare($Query);
+        $Req->execute();
+        $GetDb = $req->fetch(PDO::FETCH_ASSOC);
+
+        return $GetDb;
+    }
+
+    public function UpdateDb($Query, $DbParam)
+    {
+        $Db=$this->connexion();
+        $Req=$Db->prepare($Query);
+        $Req->execute(array($DbParam));
+    }
+
+    public function DeleteDb($DbName, $id, $DelId)
+    {
+        $Db=$this->connexion();
+        $Req=$Db->prepare('DELETE FROM'.$Arg1.'WHERE'.$id.'='.$Arg3);
+        $Req->execute();
+    }
+
+    private function connexion() {
+        try {
+            $Db = new PDO('mysql:host='.$this->connec_host.';dbname='.$this->connec_dbname, $this->connec_pseudo, $this->connec_mdp);
+            $Db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+        catch(PDOException $e) {
+            die('<h3>Erreur!</h3>');
+        }
+        return $bdd;
+    }
+
+}
+
+
+?>
