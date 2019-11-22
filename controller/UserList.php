b421b7e94e96bf2d9833f2fe7e3a4b7e4e42946f
Yannis Rekkas <yannis.rekkas@gmail.com>
2019-11-22 14:56:06 +0100

    Update UserList.php

--- controller/UserList.php
+++ controller/UserList.php
@@ -7,20 +7,8 @@ Class Userlist
     public function WatchUserList()
     {
         $UserList=\model\User::GetAllUser();
-        echo '    <header>
-                    <div class="container">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <img class="img-responsive" src="img/profile.png" alt="">
-                                <div class="intro-text">';
-        foreach($UserList as $UserList){
-            echo'<p>id = '.$UserList['id'].'</p>';
-            echo'<p>first_name ='.$UserList['first_name'].'</p>';
-            echo'<p>last_name'.$UserList['last_name'].'</p>';
-            echo'<p>'.$UserList['mail'].'</p>';
-            echo'<p>Phone ='.$UserList['phone'].'</p>';
-        }
-        echo '</div></div></div></div></header>';
+        var_dump($UserList);
+        
     }
     public function test(){
         $idtest=1;
